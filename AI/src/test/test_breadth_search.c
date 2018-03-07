a8e838437011092122ceef235a4ca054f20b10c3
mlibby <m@mlibby.com>
2018-03-07 15:25:02 -0600

    add test for breadth search to find shortest path; fix include path in vs project

--- AI/src/test/test_breadth_search.c
+++ AI/src/test/test_breadth_search.c
@@ -72,7 +72,7 @@ test_breadth_search_single_segment()
 	ASSERT(0 == strcmp("TO", solution->state));
 	ASSERT(0 == strcmp("TO", solution->action));
 	ASSERT(1 == solution->path_cost);
-	
+
 	search_node *parent = solution->parent;
 	ASSERT(NULL == parent->parent);
 	ASSERT(0 == strcmp("FROM", parent->state));
@@ -83,7 +83,35 @@ test_breadth_search_single_segment()
 	graph_free(square);
 }
 
+void
+test_breadth_search_one_shortest_path()
+{
+	graph_segment segments[] = {
+		{ "FROM", "MID1", 1, GRAPH_SEGMENT_DIRECTED },
+		{ "FROM", "MID2", 1, GRAPH_SEGMENT_DIRECTED },
+		{ "MID2", "TO", 1, GRAPH_SEGMENT_DIRECTED },
+		{ "MID1", "MID3", 1, GRAPH_SEGMENT_DIRECTED },
+		{ "MID3", "TO", 1, GRAPH_SEGMENT_DIRECTED },
+		{ NULL, NULL, 0, 0 }
+	};
 
+	graph *square = graph_create(segments);
+	breadth_search *search = breadth_search_new(square);
+	search_node *solution = breadth_search_search(search, "FROM", "TO");
+
+	ASSERT(NULL != solution);
+	ASSERT(0 == strcmp("TO", solution->state));
+
+	search_node *mid = solution->parent;
+	ASSERT(0 == strcmp("MID2", mid->state));
+
+	search_node *from = mid->parent;
+	ASSERT(0 == strcmp("FROM", from->state));
+	ASSERT(NULL == from->parent);
+
+	breadth_search_free(search);
+	graph_free(square);
+}
 
 void
 test_breadth_search_all()
@@ -91,5 +119,6 @@ test_breadth_search_all()
 	test_breadth_search_new();
 	test_breadth_search_empty_frontier_fails();
 	test_breadth_search_from_to_same();
+	test_breadth_search_one_shortest_path();
 	test_breadth_search_single_segment();
 }
